fbbb8d1c3bc09037be898bb03e5832cb3b702f42
karuna <vkkaruna@gmail.com>
2015-10-10 20:19:18 +0530

    Infinijith Sample

---
+++ web-app/app.js
@@ -0,0 +1,56 @@
+/**
+ * Created by Santhosh on 10-Oct-15.
+ */
+angular.module("mainApp", ["kendo.directives"]).controller("GridController", function($scope) {
+    $scope.mainGridOptions = {
+        dataSource: {
+            type: "json",
+            transport: {
+                read: "http://localhost:3000/users"
+            },
+            pageSize: 5,
+            serverPaging: true,
+            serverSorting: true,
+            group:{
+                field:'sport'
+            }
+        },
+        sortable: true,
+        pageable: true,
+        height:600,
+        groupable:true,
+        columns: [{
+            field: "athlete",
+            title: "Athlete Name",
+            width: "120px"
+        },{
+            field: "age",
+            title: "Age",
+            width: "120px"
+        },{
+            field: "country",
+            title: "Country",
+            width: "120px"
+        },{
+            field: "sport",
+            title: "Sport",
+            width: "120px"
+        },{
+            field: "gold",
+            title: "Gold Metals",
+            width: "120px"
+        },{
+            field: "silver",
+            title: "Silver Metals",
+            width: "120px"
+        },{
+            field: "bronze",
+            title: "Bronze Metals",
+            width: "120px"
+        },{
+            field: "total",
+            title: "Total    Metals",
+            width: "120px"
+        }]
+    };
+});
